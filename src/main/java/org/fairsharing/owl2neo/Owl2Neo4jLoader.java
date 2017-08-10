1823e459ede338f70ec2080f6bc82c9a8a6a17e9
zigur <massimorgon@gmail.com>
2017-08-10 18:03:29 +0100

    implemented basic importer (no labels)

--- src/main/java/org/fairsharing/owl2neo/Owl2Neo4jLoader.java
+++ src/main/java/org/fairsharing/owl2neo/Owl2Neo4jLoader.java
@@ -2,17 +2,15 @@ package org.fairsharing.owl2neo;
 
 import openllet.owlapi.OpenlletReasonerFactory;
 import org.apache.commons.cli.*;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.index.UniqueFactory;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
+import org.semanticweb.owlapi.search.EntitySearcher;
 import org.semanticweb.owlapi.util.OWLAPIStreamUtils;
 
 import javax.inject.Inject;
@@ -23,6 +21,9 @@ public class Owl2Neo4jLoader {
 
     private static final int OK_STATUS = 0;
     private static final int ERR_STATUS = 1;
+    private static final String HASH = "#";
+    private static final String GREATER_THAN = ">";
+    private static final String IS_A = "isA";
 
     private GraphDatabaseService graphDb;
 
@@ -61,8 +62,48 @@ public class Owl2Neo4jLoader {
             Node thingNode = getOrCreateWithUniqueFactory("owl:thing");
             for (OWLClass c : OWLAPIStreamUtils.asList(ontology.classesInSignature())) {
                 String classString = c.toString();
+                if (classString.contains(HASH)) {
+                    classString = classString.substring(classString.indexOf(HASH)+1, classString.indexOf(GREATER_THAN));
+                }
+                IRI iri = c.getIRI();
+                String iriString = iri.getIRIString();
+                for (OWLAnnotationProperty property: OWLAPIStreamUtils.asList(c.annotationPropertiesInSignature())) {
+                    System.out.println("Annotation property: " + property);
+                }
+                for (OWLAnnotation annotation: OWLAPIStreamUtils.asList(EntitySearcher.getAnnotations(c, ontology))) {
+                    System.out.println("Annotation: " + annotation);
+                    OWLAnnotationProperty property = annotation.getProperty();
+                    System.out.println(property.toString());
+                    OWLLiteral literal = (OWLLiteral) annotation.getValue();
+                    System.out.println(literal.getLiteral());
+                }
                 System.out.println("Current OWL class is: " + classString);
+                Node classNode = getOrCreateWithUniqueFactory(classString);
+                NodeSet<OWLClass> superClasses = reasoner.getSubClasses(c, true);
+
+                if (superClasses.isEmpty()) {
+                    classNode.createRelationshipTo(thingNode, RelationshipType.withName(IS_A));
+                }
+                else {
+                    for (org.semanticweb.owlapi.reasoner.Node<OWLClass> parentOWLNode: superClasses) {
+                        OWLClassExpression parent = parentOWLNode.getRepresentativeElement();
+                        String parentString = parent.toString();
+                        if (parentString.contains(HASH)) {
+                            parentString = parentString.substring(classString.indexOf(HASH)+1, classString.indexOf(GREATER_THAN));
+                        }
+                        Node parentNode = getOrCreateWithUniqueFactory(parentString);
+                        classNode.createRelationshipTo(parentNode, RelationshipType.withName(IS_A));
+                    }
+                }
+
+                for (org.semanticweb.owlapi.reasoner.Node<OWLNamedIndividual> ind : reasoner.getInstances(c, true)) {
+                    OWLNamedIndividual individual = ind.getRepresentativeElement();
+                    String individualString = individual.toString();
+                    System.out.println("Current individual is: " + individualString);
+                }
             }
+            tx.success();
+
         }
         finally {
             tx.close();
@@ -104,6 +145,8 @@ public class Owl2Neo4jLoader {
             System.exit(OK_STATUS);
         }
         catch (Exception e) {
+            System.err.println("Exception caught: " + e.getMessage());
+            e.printStackTrace();
             System.exit(ERR_STATUS);
         }
 
