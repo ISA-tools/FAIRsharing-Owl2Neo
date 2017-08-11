75e19d6966a8e57b441b88b59ac449724eb9b56a
zigur <massimorgon@gmail.com>
2017-08-11 19:21:28 +0100

    added class label and iri as node properties

--- src/main/java/org/fairsharing/owl2neo/Owl2Neo4jLoader.java
+++ src/main/java/org/fairsharing/owl2neo/Owl2Neo4jLoader.java
@@ -2,6 +2,7 @@ package org.fairsharing.owl2neo;
 
 import openllet.owlapi.OpenlletReasonerFactory;
 import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.index.UniqueFactory;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -15,15 +16,21 @@ import org.semanticweb.owlapi.util.OWLAPIStreamUtils;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class Owl2Neo4jLoader {
 
     private static final int OK_STATUS = 0;
     private static final int ERR_STATUS = 1;
+
     private static final String HASH = "#";
     private static final String GREATER_THAN = ">";
     private static final String IS_A = "isA";
+    private static final String PART_OF = "partOf";
+
+    private static final String GRAPH_DB_PATH = "var/graphdb";
+    private static final String OWL_THING = "owl:Thing";
 
     private GraphDatabaseService graphDb;
 
@@ -45,23 +52,23 @@ public class Owl2Neo4jLoader {
         UniqueFactory<Node> factory = new UniqueFactory.UniqueNodeFactory(graphDb, "index") {
             @Override
             protected void initialize(Node created, Map<String, Object> properties) {
-                created.setProperty("name", properties.get("name"));
+                created.setProperty("className", properties.get("name"));
             }
         };
 
         return factory.getOrCreate("name", nodeName);
     }
 
-    public void importOntology(OWLOntology ontology) throws Exception {
+    public void importOntology(OWLOntology ontology, OWLDataFactory factory) throws Exception {
         final OWLReasoner reasoner = OpenlletReasonerFactory.getInstance().createReasoner(ontology);
         if (!reasoner.isConsistent()) {
             throw new Exception("Ontology is inconsistent");
         }
         Transaction tx = graphDb.beginTx();
         try {
-            Node thingNode = getOrCreateWithUniqueFactory("owl:thing");
+            Node thingNode = getOrCreateWithUniqueFactory(OWL_THING);
             for (OWLClass c : OWLAPIStreamUtils.asList(ontology.classesInSignature())) {
-                String classString = c.toString();
+                String classString = c.toString(), classLabel = classString;
                 if (classString.contains(HASH)) {
                     classString = classString.substring(classString.indexOf(HASH)+1, classString.indexOf(GREATER_THAN));
                 }
@@ -70,15 +77,18 @@ public class Owl2Neo4jLoader {
                 for (OWLAnnotationProperty property: OWLAPIStreamUtils.asList(c.annotationPropertiesInSignature())) {
                     System.out.println("Annotation property: " + property);
                 }
-                for (OWLAnnotation annotation: OWLAPIStreamUtils.asList(EntitySearcher.getAnnotations(c, ontology))) {
+                for (OWLAnnotation annotation: OWLAPIStreamUtils.asList(EntitySearcher.getAnnotations(c, ontology, factory.getRDFSLabel()))) {
                     System.out.println("Annotation: " + annotation);
                     OWLAnnotationProperty property = annotation.getProperty();
                     System.out.println(property.toString());
                     OWLLiteral literal = (OWLLiteral) annotation.getValue();
                     System.out.println(literal.getLiteral());
+                    classLabel = literal.getLiteral();
                 }
                 System.out.println("Current OWL class is: " + classString);
                 Node classNode = getOrCreateWithUniqueFactory(classString);
+                classNode.setProperty("name", classLabel);
+                classNode.setProperty("iri", iriString);
                 NodeSet<OWLClass> superClasses = reasoner.getSubClasses(c, true);
 
                 if (superClasses.isEmpty()) {
@@ -92,7 +102,7 @@ public class Owl2Neo4jLoader {
                             parentString = parentString.substring(classString.indexOf(HASH)+1, classString.indexOf(GREATER_THAN));
                         }
                         Node parentNode = getOrCreateWithUniqueFactory(parentString);
-                        classNode.createRelationshipTo(parentNode, RelationshipType.withName(IS_A));
+                        parentNode.createRelationshipTo(classNode, RelationshipType.withName(PART_OF));
                     }
                 }
 
@@ -102,6 +112,9 @@ public class Owl2Neo4jLoader {
                     System.out.println("Current individual is: " + individualString);
                 }
             }
+
+
+
             tx.success();
 
         }
@@ -134,14 +147,23 @@ public class Owl2Neo4jLoader {
         }
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 
+        try {
+            System.out.println("Deleting graph database directory");
+            FileUtils.deleteDirectory(new File(GRAPH_DB_PATH));
+        }
+        catch (IOException e) {
+            System.err.println("Exception caught: " + e.getMessage());
+        }
+
 
         File file = new File(cmd.getOptionValue("o").trim());
         try {
             OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+            OWLDataFactory factory = manager.getOWLDataFactory();
             System.out.println("Loaded ontology" + ontology);
-            GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File("var/graphdb"));
+            GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File(GRAPH_DB_PATH));
             Owl2Neo4jLoader loader = new Owl2Neo4jLoader(graphDb);
-            loader.importOntology(ontology);
+            loader.importOntology(ontology, factory);
             System.exit(OK_STATUS);
         }
         catch (Exception e) {
